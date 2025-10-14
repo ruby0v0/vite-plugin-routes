6d8f63ab91025a3328bb4e33a71e7cfbf59ee59c
鲨鱼辣椒 <monishine_code@163.com>
2025-10-14 23:17:40 +0800

    feat: 搭建框架

---
+++ rollup.config.mjs
@@ -0,0 +1,27 @@
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
+import resolve from '@rollup/plugin-node-resolve'
+import terser from '@rollup/plugin-terser'
+
+/** @type {import('rollup').RollupOptions} */
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.cjs.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'es',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    json(),
+    terser(),
+  ],
+}
