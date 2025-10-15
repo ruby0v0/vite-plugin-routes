d442b055c841054aa90f083efe57c57d406e766a
鲨鱼辣椒 <monishine_code@163.com>
2025-10-15 10:27:00 +0800

    fix: 修复lint报错

--- examples/vue/vite.config.ts
+++ examples/vue/vite.config.ts
@@ -3,7 +3,5 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue({
-    include: [/\.vue$/, /\.md$/],
-  })],
+  plugins: [vue()],
 })
